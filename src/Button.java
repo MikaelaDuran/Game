002188b871335265a51483ae4454bef9c0e0972c
Matilda Erenius <matildaerenius@hotmail.com>
2024-10-30 15:44:41 +0100

    1# started on Button

--- src/Button.java
+++ src/Button.java
@@ -1,2 +1,10 @@
-public class Button {
+import javax.swing.*;
+import java.awt.*;
+
+// Klass för att skapa knapparna i spelet
+public class Button extends JButton {
+    public Button(String text) {
+        super(text);
+        setBackground(Color.gray);
+    }
 }
