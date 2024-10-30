f8f1d38755b9b3e8c0e87e1a5a48eb143cc14b7a
Mikaela Duran <mikaeladuran93@gmail.com>
2024-10-30 15:02:48 +0100

    Initial commit StartFrame

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -1,2 +1,24 @@
-public class StartFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class StartFrame extends JFrame {
+    private JTextField nameField;
+
+
+    public StartFrame() {
+        setTitle("Number Puzzle");
+        ImageIcon puzzle = new ImageIcon("");
+        setIconImage(puzzle.getImage());
+        setSize(300, 150);
+        setResizable(false);
+        setFocusable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setBackground(Color.lightGray);
+
+        JLabel label = new JLabel("Enter your name:");
+        nameField = new JTextField(20);
+        JButton playButton = new JButton("PLAY");
+
+    }
 }
